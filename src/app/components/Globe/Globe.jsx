d84c2e27df018b527e282341e7cdb9b8e06f9db3
delwerhossain <delwerhossain006@gmail.com>
2023-08-22 12:49:18 +0100

    update for  optimization purposes

--- src/app/components/Globe/Globe.jsx
+++ src/app/components/Globe/Globe.jsx
@@ -32,7 +32,7 @@ export const Globe = () => {
         // Called on every animation frame.
         // `state` will be an empty object, return updated params.
         state.phi = phi;
-        phi += 0.01;
+        phi += 0.001;
       },
     });
 
