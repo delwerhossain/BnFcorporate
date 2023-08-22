ab0ada0c30d73f9d05cb4ebdca3fc97aeba05ef5
delwerhossain <delwerhossain006@gmail.com>
2023-08-22 15:20:10 +0100

    font change

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,6 @@
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: "B & F CORPORATE",
@@ -13,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
-      <body className={inter.className}>
+      <body >
         <Header />
         <div className="max-w-7xl mx-auto">{children}</div>
         <Footer />
