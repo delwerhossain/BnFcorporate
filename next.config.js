a54c0198a3bd0196bfdc350107e7c8c3254c3670
delwerhossain <delwerhossain006@gmail.com>
2023-10-16 09:41:18 +0100

    config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   images: {
     domains: ["video.chowdhuryinfotech.com"],
   },
+  // The output directory
+  outputDirectory: "./build",
+
+  // The optimization settings
+  optimization: {
+    // Enable minification
+    minimize: true,
+  },
+
+  // The plugins that are used
+  plugins: [
+    // Enable image optimization
+    ["next-transpile-modules", { transpileModules: ["next/image"] }],
+  ],
 };
 
 module.exports = nextConfig;
